a2435f107ba19185641606c74e1f33efc72e5618
Ardikurnia1 <133620481+Ardikurnia1@users.noreply.github.com>
2023-06-04 22:57:09 +0700

    Add files via upload

---
+++ Ardi_2113010248.java
@@ -0,0 +1,6 @@
+public class Ardi_2113010248 {
+   public static void main(String[] args) {
+     System.out.println("Nama : Ardi Kurniawan");
+     System.out.println("NIM  : 2113010248");
+   }
+}

267f875cc91fd1e8bfcc1c7ca1af4b200fc9b8c9
RohaniNA <133620679+RohaniNA@users.noreply.github.com>
2023-08-07 11:28:30 -0700

    Add files via upload

---
+++ Revisi_Mandiri_RohaniNA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package revisi_rohanina;
+
+/**
+ *
+ * @author MG GAMING PRO
+ */
+public class Revisi_Mandiri_RohaniNA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("Nama : Rohani Nur Ahmadi");
+     System.out.println("NIM  : 2113010256");
+   }
+}
